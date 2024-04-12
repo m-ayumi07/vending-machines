cb5a39e0d520f2f1f73bcc2534faac9e89cda734
ayumi matsushita <aym.tksg.bbc1024@gmail.com>
2024-04-12 18:03:51 +0900

    New Create

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -7,7 +7,6 @@ use App\Http\Requests\Auth\LoginRequest;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Session;
 
 class AuthenticatedSessionController extends Controller
 {
@@ -32,12 +31,6 @@ class AuthenticatedSessionController extends Controller
         $request->authenticate();
 
         $request->session()->regenerate();
-
-        $url = Session::get('_previous.url');
-
-        if($url){
-            return redirect()->to($url);
-        }
 
         return redirect()->intended(RouteServiceProvider::HOME);
     }
