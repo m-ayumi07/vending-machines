8e727e520a3db7711eb4144b9370fe6f63172af6
ayumi matsushita <aym.tksg.bbc1024@gmail.com>
2024-05-28 18:25:40 +0900

    New create

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use App\Http\Controllers\SaleController;
 */
 
 Auth::routes();
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/', [ProductController::class, 'index'])->name('products.index');
+    Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
+    Route::post('/products', [ProductController::class, 'store'])->name('products.store');
+    Route::get('/products/{product}', [ProductController::class, 'show'])->name('products.show');
+    Route::get('/products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
+    Route::put('/products/{product}', [ProductController::class, 'update'])->name('products.update');
+    Route::delete('/products/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
+});
